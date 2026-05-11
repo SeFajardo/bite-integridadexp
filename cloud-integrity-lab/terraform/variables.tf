6268f55bd50889ac3e13258b464654548d780ac5
SeFajardo <s.fajardog@uniandes.edu.co>
2026-05-11 14:08:31 -0500

    variables tf

--- cloud-integrity-lab/terraform/variables.tf
+++ cloud-integrity-lab/terraform/variables.tf
@@ -8,15 +8,17 @@ variable "db_password" {
   description = "PostgreSQL password for the cloud_integrity_db user"
   type        = string
   sensitive   = true
+  default     = "BitePass2025"
 }
 
 variable "key_name" {
   description = "Name of the EC2 key pair to attach to the instances"
   type        = string
+  default     = "vockey"
 }
 
 variable "repo_url" {
   description = "Public Git URL of the cloud-integrity-lab repository"
   type        = string
-  default     = "https://github.com/your-user/cloud-integrity-lab.git"
+  default     = "https://github.com/SeFajardo/bite-integridadexp.git"
 }
